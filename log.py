abdac51eae4b150de2dbccc6442c77a8920bece1
Godwinhos <godwinbenjamin2x18.com>
2025-07-16 21:35:04 +0100

    operands and operations

---
+++ log.py
@@ -0,0 +1,37 @@
+'''
+a = 5
+b  = 2
+print(a // b)
+print(10//3)
+print(10/3)
+print(5.//2)
+'''
+print("addition")
+first_number = 5
+first_number+=10
+print(first_number)
+
+print("division")
+second_number =15
+second_number/= 5
+print(second_number)
+
+print("multiplication")
+third_number = 5
+third_number *= 5
+print(third_number)
+
+print("subtraction")
+fourth_number = 15
+fourth_number -= 10
+print(fourth_number)
+
+print("floor division")
+fifth_number = 5
+fifth_number //= 3
+print(fifth_number)
+
+print("sixth_number")
+sixth_number = 4
+sixth_number %=2
+print(sixth_number)
